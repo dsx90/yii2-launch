512c46039c75f132f351aa9f7b9f0250fb194d25
ilchenko.m.a <ilchenko.m.a@avtomir.ru>
2018-08-14 14:40:15 +0300

    fix left buttom panel

--- src/views/launch/_form.php
+++ src/views/launch/_form.php
@@ -17,129 +17,125 @@ use dsx90\launcher\models\Module;
 <div class="launch-form">
 
     <?php $form = ActiveForm::begin(); ?>
-    <div class="row">
-        <div class="col-sm-12">
-            <?//php $this->beginBlock('control-panel') ?>
-                <div class="control-panel pull-right">
-                    <?= $form->field($model, 'status')->widget(\dosamigos\switchinput\SwitchBox::className(),[
-                        'clientOptions' => [
-                            'size' => 'normal',
-                            'onColor' => 'success',
-                            'offColor' => 'danger',
-                        ],
-                        'inlineLabel' => false
-                    ])->label(false);?>
-                    <?= Html::submitButton('<i class="glyphicon glyphicon-floppy-disk"></i> '.Yii::t('backend', 'Save'), ['class' => 'btn btn-primary']) ?>
-                    <?php if (!$model->isNewRecord) {
-                        echo Html::a('<i class="glyphicon glyphicon-eye-open"></i> '.Yii::t('backend', 'View'), ['view', 'id' => $model->id], [
-                                'class' => 'btn btn-success',
-                            ])." ";
-                        echo Html::a('<i class="glyphicon glyphicon-trash"></i> '.Yii::t('backend', 'Delete'), ['delete', 'id' => $model->id], [
-                                'class' => 'btn btn-danger',
-                                'data' => [
-                                    'confirm' => Yii::t('backend', 'Are you sure you want to delete the resources?'),
-                                    'method' => 'post',
-                                ],
-                            ])." ";
-                        echo Html::a('<i class="glyphicon glyphicon-level-up"></i> '.Yii::t('backend', 'Create child'), ['create', 'parent_id' => $model->parent_id], [
-                                'class' => 'btn btn-default',
-                            ])." ";
-                    }
-                    ?>
-                    <?= Html::a('<i class="fa fa-chevron-left"></i>',['update', 'id' => $model->getPrev()], ['class' => 'btn btn-default',]) ?>
-                    <?= Html::a('<i class="fa fa-arrow-up"></i>',['index'], ['class' => 'btn btn-default',]) ?>
-                    <?= Html::a('<i class="fa fa-chevron-right"></i>',['update', 'id' => $model->getNext()], ['class' => 'btn btn-default',]) ?>
-                </div>
-            <?//php $this->endBlock() ?>
 
-        </div>
-        <div id="launch-row">
-            <div id="launch-left" class="col-md-9">
-                <div class="border-field">
-
-                    <?= $form->field($model, 'title')->textInput(['maxlength' => true])/*->hint('Длинна пароля не меньше 70 символов.')*/ ?>
-
-                    <?= $form->field($model, 'longtitle')->textInput(['maxlength' => true]) ?>
-
-                    <?= $form->field($model, 'description')->textarea(['rows' => 5]) ?>
-
-                    <?= $form->field($model, 'keywords')->textInput(['maxlength' => true]) ?>
-
-                </div>
-                <div class="adver-view">
-                    <i>Видимость в поисковиках</i>
-                    <h2><span id="adver-title"><?= $model->title ? $model->title : 'Титул' ?></span>: <span id="adver-longtitle"><?= $model->longtitle ?: 'Краткое описание' ?></span></h2>
-                    <h4><?= env('FRONTEND_URL')?>›<span id="url"><?= $model->slug ?: 'url ссылка' ?></span></h4>
-                    <h5><span id="adver-description"><?= $model->description ?: 'Обьявление' ?></span></h5>
-                </div>
-            </div>
-            <div id="launch-right" class="col-md-3">
-                <?= $form->field($model, 'parent_id')->dropDownList(\dsx90\launcher\models\Launch::getAll(),
-                    ['prompt' => 'Нет']) ?>
-
-                <?= $form->field($model, 'menutitle', [
-                    'addon' => [
-                        'append' => [
-                            'content'=> Html::a(Yii::t('backend', 'Repeat the name'), '#', [
-                                'class' =>['btn btn-default repeat-name']]),
-                            'asButton'=>true,
-                        ],
-                        'groupOptions' => [
-                            'id' => 'title-btn'
-                        ]
-                    ]
-                ]); ?>
-
-                <?= $form->field($model, 'slug', [
-                    'addon' => [
-                        'append' => [
-                            'content' => ButtonDropdown::widget([
-                                'label' => Yii::t('backend', 'From'),
-                                'dropdown' => [
-                                    'items' => [
-                                        ['label' => Yii::t('backend', 'From the title'), 'url' => '#', 'options' => ['class'=>'translate-name']],
-                                        ['label' => Yii::t('backend', 'From the menutitle'), 'url' => '#', 'options' => ['class'=>'translate-title']],
+        <div class="row">
+            <div class="col-sm-12">
+                <?//php $this->beginBlock('control-panel') ?>
+                    <div class="control-panel pull-right">
+                        <?= $form->field($model, 'status')->widget(\dosamigos\switchinput\SwitchBox::className(),[
+                            'clientOptions' => [
+                                'size' => 'normal',
+                                'onColor' => 'success',
+                                'offColor' => 'danger',
+                            ],
+                            'inlineLabel' => false
+                        ])->label(false);?>
+                        <?= Html::submitButton('<i class="glyphicon glyphicon-floppy-disk"></i> '.Yii::t('backend', 'Save'), ['class' => 'btn btn-primary']) ?>
+                        <?php if (!$model->isNewRecord) {
+                            echo Html::a('<i class="glyphicon glyphicon-eye-open"></i> '.Yii::t('backend', 'View'), ['view', 'id' => $model->id], [
+                                    'class' => 'btn btn-success',
+                                ])." ";
+                            echo Html::a('<i class="glyphicon glyphicon-trash"></i> '.Yii::t('backend', 'Delete'), ['delete', 'id' => $model->id], [
+                                    'class' => 'btn btn-danger',
+                                    'data' => [
+                                        'confirm' => Yii::t('backend', 'Are you sure you want to delete the resources?'),
+                                        'method' => 'post',
                                     ],
-                                ],
-                                'options' => ['class'=>'btn-default']
-                            ]),
-                            'asButton' => true
-                        ],
-                        'groupOptions' => [
-                            'id' => 'alias-btn'
+                                ])." ";
+                            echo Html::a('<i class="glyphicon glyphicon-level-up"></i> '.Yii::t('backend', 'Create child'), ['create', 'parent_id' => $model->parent_id], [
+                                    'class' => 'btn btn-default',
+                                ])." ";
+                        }
+                        ?>
+                        <?= Html::a('<i class="fa fa-chevron-left"></i>',['update', 'id' => $model->getPrev()], ['class' => 'btn btn-default',]) ?>
+                        <?= Html::a('<i class="fa fa-arrow-up"></i>',['index'], ['class' => 'btn btn-default',]) ?>
+                        <?= Html::a('<i class="fa fa-chevron-right"></i>',['update', 'id' => $model->getNext()], ['class' => 'btn btn-default',]) ?>
+                    </div>
+                <?//php $this->endBlock() ?>
+
+            </div>
+            <div id="launch-row">
+                <div id="launch-left" class="col-md-9">
+                    <div class="border-field">
+
+                        <?= $form->field($model, 'title')->textInput(['maxlength' => true])/*->hint('Длинна пароля не меньше 70 символов.')*/ ?>
+
+                        <?= $form->field($model, 'longtitle')->textInput(['maxlength' => true]) ?>
+
+                        <?= $form->field($model, 'description')->textarea(['rows' => 5]) ?>
+
+                        <?= $form->field($model, 'keywords')->textInput(['maxlength' => true]) ?>
+
+                    </div>
+                    <div class="adver-view">
+                        <i>Видимость в поисковиках</i>
+                        <h2><span id="adver-title"><?= $model->title ? $model->title : 'Титул' ?></span>: <span id="adver-longtitle"><?= $model->longtitle ?: 'Краткое описание' ?></span></h2>
+                        <h4><?= env('FRONTEND_URL')?>›<span id="url"><?= $model->slug ?: 'url ссылка' ?></span></h4>
+                        <h5><span id="adver-description"><?= $model->description ?: 'Обьявление' ?></span></h5>
+                    </div>
+                </div>
+                <div id="launch-right" class="col-md-3">
+                    <?= $form->field($model, 'parent_id')->dropDownList(\dsx90\launcher\models\Launch::getAll(),
+                        ['prompt' => 'Нет']) ?>
+
+                    <?= $form->field($model, 'menutitle', [
+                        'addon' => [
+                            'append' => [
+                                'content'=> Html::a(Yii::t('backend', 'Repeat the name'), '#', [
+                                    'class' =>['btn btn-default repeat-name']]),
+                                'asButton'=>true,
+                            ],
+                            'groupOptions' => [
+                                'id' => 'title-btn'
+                            ]
                         ]
-                    ]
-                ]); ?>
+                    ]); ?>
 
-                <?//= $form->field($model, 'author_id')->dropDownList($model->getAuthor()->id)?> <!--TODO: Вывести Автора-->
+                    <?= $form->field($model, 'slug', [
+                        'addon' => [
+                            'append' => [
+                                'content' => ButtonDropdown::widget([
+                                    'label' => Yii::t('backend', 'From'),
+                                    'dropdown' => [
+                                        'items' => [
+                                            ['label' => Yii::t('backend', 'From the title'), 'url' => '#', 'options' => ['class'=>'translate-name']],
+                                            ['label' => Yii::t('backend', 'From the menutitle'), 'url' => '#', 'options' => ['class'=>'translate-title']],
+                                        ],
+                                    ],
+                                    'options' => ['class'=>'btn-default']
+                                ]),
+                                'asButton' => true
+                            ],
+                            'groupOptions' => [
+                                'id' => 'alias-btn'
+                            ]
+                        ]
+                    ]); ?>
 
-                <?//= $form->field($model, 'published_at')->widget(DateTimeWidget::className(), ['phpDatetimeFormat' => 'dd.MM.yyyy, HH:mm:ss']) ?>
+                    <?= $form->field($model, 'author_id')->dropDownList(\yii\helpers\ArrayHelper::map($model->author, 'username', $model->author_id))?> <!--TODO: Вывести Автора-->
 
-                <?= $form->field($model, 'module_id')->dropDownList(Module::getAll(),
-                    ['prompt' => 'Без типа:']) ?>
+                    <?//= $form->field($model, 'published_at')->widget(DateTimeWidget::className(), ['phpDatetimeFormat' => 'dd.MM.yyyy, HH:mm:ss']) ?>
 
-                <?= $form->field($model, 'template_id')->dropDownList(Template::getAll(),
-                    ['prompt' => 'Пустой шаблон:']) ?>
+                    <?= $form->field($model, 'module_id')->dropDownList(Module::getAll(),
+                        ['prompt' => 'Без типа:']) ?>
+
+                    <?= $form->field($model, 'template_id')->dropDownList(Template::getAll(),
+                        ['prompt' => 'Пустой шаблон:']) ?>
+                </div>
             </div>
         </div>
-    </div>
 
-    <?php Pjax::begin([
-        'linkSelector' => false,
-        'formSelector' => false,
-        'id' => 'module'
-    ]) ?>
+        <?php Pjax::begin(['linkSelector' => false, 'formSelector' => false, 'id' => 'module']) ?>
+            <div id="fields" class="forms">
+                <?if ($model->module) {
+                    echo $this->renderAjax($model->module->form, [
+                        'form' => $form,
+                        'model' => $model->models ?: (new $model->module->model)
+                    ]);
+                }?>
+            </div>
+        <?php Pjax::end() ?>
 
-    <div id="fields" class="forms">
-        <?if ($model->module) {
-            echo $this->renderAjax($model->module->form, [
-                'form' => $form,
-                'model' => $model->models ?: (new $model->module->model)
-            ]);
-        }?>
-    </div>
-
-    <?php Pjax::end() ?>
     <?php ActiveForm::end()?>
 
 
@@ -161,13 +157,13 @@ $this->registerJs(<<<JS
     $('.translate-title').click(function(){
         var text = $('#launch-menutitle').val().toLowerCase();
         result = translit(text);
-    $('#launch-slug').val(result);
+        $('#launch-slug').val(result);
     });
     $('#launch-module_id').on('change', function(){
-    $.pjax.reload('#module', {
-        'url': window.location.href.replace(/&module=[0-9]+/g, '') + '&module=' + $(this).val(),
-        'replace': false
-    })
+        $.pjax.reload('#module', {
+            'url': window.location.href.replace(/&module=[0-9]+/g, '') + '&module=' + $(this).val(),
+            'replace': false
+        })
     });
     $('#launch-title').keyup(function() {
       $('#adver-title')[0].innerHTML = $(this)[0].value
